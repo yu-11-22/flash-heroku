c20712f0a96b84b4be4de8f8c90618ea9b9facef
willy24692485 <willy24692485@gmail.com>
2021-09-15 21:24:10 +0800

    First deploy

---
+++ app.py
@@ -0,0 +1,13 @@
+from flask import Flask
+app=Flask(__name__) # __name__ 代表目前執行的模組
+
+@app.route("/") #函式的裝飾(Decorator):以函式為基礎,提供附加的功能
+def home():
+    return "Hello Flask"
+
+@app.route("/test")
+def test():
+    return "This is test"
+
+if __name__=="__main__":    # 如果以主程式執行
+    app.run()   #立刻啟動伺服器
